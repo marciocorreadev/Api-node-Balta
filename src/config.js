831e8ac0ff29b4f1d83325822d278a91d4708dd0
Márcio <msc@outlook.com.br>
2020-09-01 21:53:21 -0300

    add-token

--- src/config.js
+++ src/config.js
@@ -2,12 +2,11 @@
 
 'use strict';
 
-global.SALT_KEY = 'F5BSD575-DASdaSD-DASdas-dASDASd45';
+global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo a node Store!';
 
 module.exports = {
-  connectionString:
-    'mongodb+srv://marcio:marcio@cluster0.kiaxr.azure.mongodb.net/nodestr?retryWrites=true&w=majority',
-  sendgridKey: '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',
+  connectionString: '',
+  sendgridKey: '',
   containerConnectionString: 'TBD',
 };
