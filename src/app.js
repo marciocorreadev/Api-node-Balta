ae6272187e3152130939d26579bea25f3343b6d9
Márcio <msc@outlook.com.br>
2020-09-01 21:59:55 -0300

    add-token

--- src/app.js
+++ src/app.js
@@ -16,13 +16,22 @@ const Product = require('./models/product.js');
 const Costumer = require('./models/costumer.js');
 const Order = require('./models/order.js');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const index = require('./routes/index-route');
 const product = require('./routes/product-route');
 const customer = require('./routes/customer-route');
 const order = require('./routes/order-route');
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,x-access-token'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+});
 
 app.use('/', index);
 app.use('/products', product);
